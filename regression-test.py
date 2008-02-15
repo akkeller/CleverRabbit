956b2ab8c5af62767f160bf4b9998ef938c0a153
Karl Keller <akarlk@gmail.com>
2008-02-14 20:31:21 -0600

    Stub of regression-test.py.

---
+++ regression-test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#
+#  regression-test.py
+#
+#   Copyright (c) 2008 A. Karl Keller (http://karlk.net)
+#
+#   This code is open-source, free software, made available without warranty under
+#   the terms of the GNU General Public License, either version 2 or later (see 
+#   http://www.gnu.org/licenses/gpl.html or included copy); as such, it may be 
+#   redistributed and/or modified in accordance with that document.
+#
+
+import os
+
+TRANSLITERATE = "./build/transliterate"
+INPUT_DEFINITION_PATH = "./active_definitions/Phonemic.rtkinput"
+SCRIPT_DEFINITION_PATH = "./active_definitions/Krung.rtkscript"
+ENCODING_DEFINITION_PATH = "./active_definitions/Unicode.rtkfont"
+
